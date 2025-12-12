ead800b9cbe644dca558fd44b8206d81b9d64e40
d24507876-spec <d24507876@gmail.com>
2025-12-12 23:22:30 +0530

    Initial commit

---
+++ ProductAPI/Controllers/ProductController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProductAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private static readonly List<Product> products = new()
+        {
+            new Product { Id = 1, Name = "Laptop", Price = 50000 },
+            new Product { Id = 2, Name = "Mobile", Price = 20000 }
+        };
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return products;
+        }
+
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
+            product.Id = products.Count + 1;
+            products.Add(product);
+            return CreatedAtAction(nameof(Get), new { id = product.Id }, product);
+        }
+    }
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+    }
+}
