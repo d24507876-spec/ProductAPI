27e0af765f41a29546bc31679003b01b555d53f6
d24507876-spec <d24507876@gmail.com>
2025-12-13 00:56:51 +0530

    Update pipeline and use environment variable for SQL password

--- ProductAPI/Program.cs
+++ ProductAPI/Program.cs
@@ -8,8 +8,12 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 
+var sqlPassword = Environment.GetEnvironmentVariable("SQL_PASSWORD");
+var connectionString = $"Server=tcp:dbfirst1000.database.windows.net,1433;Initial Catalog=ProductDB;Persist Security Info=False;User ID=admindb100;Password={sqlPassword};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+
+
 builder.Services.AddDbContext<ProductDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("connectionString")));
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
